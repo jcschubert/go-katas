312f2cb23e8385c9ef199b1b5a5213c3e0f9d640
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-01 14:41:11 +0100

    WeirdStringCase Kata: Extracted wordToWeirdCase

--- weird-strings/weird-strings.go
+++ weird-strings/weird-strings.go
@@ -9,20 +9,25 @@ import (
 // and all odd strings in each word lower cased
 func toWeirdCase(str string) string {
 	words := strings.Split(str, " ")
-
 	for i := 0; i < len(words); i++ {
-		var chars []string = strings.Split(words[i], "")
 
-		for i := 0; i < len(chars); i++ {
-			if i%2 == 0 {
-				chars[i] = strings.ToUpper(chars[i])
-			} else {
-				chars[i] = strings.ToLower(chars[i])
-			}
-		}
-
-		words[i] = strings.Join(chars, "")
+		words[i] = wordToWeirdCase(words[i])
 	}
 
 	return strings.Join(words, " ")
+}
+
+// wordToWeirdCase turns a single word to WeIrD CaSe
+func wordToWeirdCase(w string) string {
+	var chars []string = strings.Split(w, "")
+
+	for i := 0; i < len(chars); i++ {
+		if i%2 == 0 {
+			chars[i] = strings.ToUpper(chars[i])
+		} else {
+			chars[i] = strings.ToLower(chars[i])
+		}
+	}
+
+	return strings.Join(chars, "")
 }
