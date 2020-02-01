0110a8211dd8b20d0c2108f714d69be2e53e299f
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-01 15:33:56 +0100

    Split Strings: Refactoring

--- split-strings/split-strings.go
+++ split-strings/split-strings.go
@@ -1,8 +1,23 @@
 package splitstrings
 
+// Solution splits the string into pais of two characters. If the string contains
+// an odd number of characters then it the missing second character of the pair
+// is replaced with an underscore ("_")
+//
+// Examples:
+// Solution("abc") // should return ["ab", "c_"]
+// Solution("abcd") // should return ["ab", "cd"]
+//
 func Solution(str string) []string {
+	result := []string{}
+
+	item := str
+
 	if len(str) < 2 {
-		return []string{str + "_"}
+		item = item + "_"
 	}
-	return []string{"ab"}
+
+	result = append(result, item)
+
+	return result
 }
