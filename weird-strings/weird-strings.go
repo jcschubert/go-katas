144cf0c01a918f3c6a8cddeb51e08ef3ea8c185a
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-01 13:50:01 +0100

    WeirdStringCase Kata: Initial commit

---
+++ weird-strings/weird-strings.go
@@ -0,0 +1,8 @@
+package weirdstrings
+
+// toWeirdCase accepts a string, and returns the same
+// string with all even index characters in each word upper cased
+// and all odd strings in each word lower cased
+func toWeirdCase(str string) string {
+	return "A"
+}
---
+++ weird-strings/weird-strings_test.go
@@ -0,0 +1,31 @@
+package weirdstrings
+
+import (
+	"testing"
+)
+
+type WeirdCaseTestCase struct {
+	input    string
+	expected string
+}
+
+func TestToWeirdCase(t *testing.T) {
+
+	testCases := []WeirdCaseTestCase{
+		{"a", "A"},
+	}
+	runWeirdCaseTestCases(t, testCases)
+}
+
+func runWeirdCaseTestCases(t *testing.T, cases []WeirdCaseTestCase) {
+	for _, tc := range cases {
+		t.Run("It turns words into weirdly cased words", func(t *testing.T) {
+			result := toWeirdCase(tc.input)
+
+			if result != tc.expected {
+				t.Logf("Given: %+v, expected: %q, got: %q", tc.input, tc.expected, result)
+				t.Fail()
+			}
+		})
+	}
+}
