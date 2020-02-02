db73fdac5a4a5a255b0a58f44b196bd8251ee384
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-02 12:18:10 +0100

    IP Validation: Refactoring

--- ip-validation/ip_validation.go
+++ ip-validation/ip_validation.go
@@ -25,15 +25,18 @@ func has4Octets(octets []string) bool {
 
 func allOctetsWithinAllowedRange(octets []string) bool {
 	for _, octet := range octets {
-		if len(octet) > 1 && octet[0] == '0' {
-			return false
-		}
-
-		intVal, err := strconv.Atoi(octet)
-
-		if err != nil || intVal < 0 || intVal > 255 {
+		if hasLeadingZero(octet) || !isWithinRange(octet) {
 			return false
 		}
 	}
 	return true
+}
+
+func hasLeadingZero(octet string) bool {
+	return len(octet) > 1 && octet[0] == '0'
+}
+
+func isWithinRange(octet string) bool {
+	intVal, err := strconv.Atoi(octet)
+	return err == nil && 0 <= intVal && intVal <= 255
 }
