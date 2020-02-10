343860e981e6e5f056a82ce790b5fb672db11fad
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-10 13:58:48 +0100

    Speedcontrol: Refactoring: replaced if with math.Max

--- speedcontrol/speedcontrol.go
+++ speedcontrol/speedcontrol.go
@@ -5,14 +5,12 @@ import "math"
 // Gps calculates the floor of the *maximum* average speed
 // per hour for of the distance covered at that particular
 // amount of seconds.
-func Gps(s int, x []float64) int {
-	max := 0.0
-	for i := 1; i < len(x); i++ {
-		d := x[i] - x[i-1]
-		avgSpeed := math.Floor((3600 * d) / float64(s))
-		if avgSpeed > max {
-			max = avgSpeed
-		}
+func Gps(interval int, distances []float64) int {
+	max, avgSpeed := 0.0, 0.0
+	for i := 1; i < len(distances); i++ {
+		d := distances[i] - distances[i-1]
+		avgSpeed = math.Floor((3600 * d) / float64(interval))
+		max = math.Max(max, avgSpeed)
 	}
 	return int(max)
 }
