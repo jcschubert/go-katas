d29e2015ac5b0f3679576ef4cb88c0c83cec83a0
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-10 13:47:34 +0100

    Speedcontrol: Handle basic test case 1

---
+++ speedcontrol/speedcontrol.go
@@ -0,0 +1,22 @@
+package speedcontrol
+
+import "math"
+
+// Gps calculates the floor of the *maximum* average speed
+// per hour for of the distance covered at that particular
+// amount of seconds.
+func Gps(s int, x []float64) int {
+	if len(x) < 2 {
+		return 0
+	}
+
+	max := 0.0
+	for i := 1; i < len(x)-1; i++ {
+		d := x[i] - x[i-1]
+		avgSpeed := math.Floor((3600 * d) / float64(s))
+		if avgSpeed > max {
+			max = avgSpeed
+		}
+	}
+	return int(max)
+}
---
+++ speedcontrol/speedcontrol_test.go
@@ -0,0 +1,24 @@
+package speedcontrol
+
+import "testing"
+
+func TestGps(t *testing.T) {
+	cases := []struct {
+		interval  int
+		distances []float64
+		expected  int
+	}{
+		{15, []float64{}, 0},
+		{15, []float64{0.0}, 0},
+		{15, []float64{0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25}, 74},
+	}
+
+	const msg string = "Gps(%d, %v) returned %d, but %d was expected."
+
+	for _, c := range cases {
+		got := Gps(c.interval, c.distances)
+		if got != c.expected {
+			t.Fatalf(msg, c.interval, c.distances, got, c.expected)
+		}
+	}
+}
