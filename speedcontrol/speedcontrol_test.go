97ef2bb39ef2537b17342624404be0567803d8c8
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-10 13:48:19 +0100

    Speedcontrol: Handle basic test case 2

--- speedcontrol/speedcontrol_test.go
+++ speedcontrol/speedcontrol_test.go
@@ -11,6 +11,7 @@ func TestGps(t *testing.T) {
 		{15, []float64{}, 0},
 		{15, []float64{0.0}, 0},
 		{15, []float64{0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25}, 74},
+		{20, []float64{0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61}, 41},
 	}
 
 	const msg string = "Gps(%d, %v) returned %d, but %d was expected."
