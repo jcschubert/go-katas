95a5683b30fad87d13e55bcdf0f6f6fe295230ca
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-10 15:18:47 +0100

    Max Length Difference: Handle empty case

---
+++ maximumlengthdifference/difference.go
@@ -0,0 +1,8 @@
+package maximumlengthdifference
+
+func MxDifLg(a1 []string, a2 []string) int {
+	if len(a1) == 0 || len(a2) == 0 {
+		return -1
+	}
+	return 0
+}
---
+++ maximumlengthdifference/difference_test.go
@@ -0,0 +1,22 @@
+package maximumlengthdifference
+
+import "testing"
+
+func TestMaxLengthDifference(t *testing.T) {
+	cases := []struct {
+		a1     []string
+		a2     []string
+		result int
+	}{
+		{[]string{}, []string{}, -1},
+	}
+
+	const msg string = "MxDifLg(%v, %v) should return %d, but returned %d instead."
+
+	for _, c := range cases {
+		got := MxDifLg(c.a1, c.a2)
+		if got != c.result {
+			t.Fatalf(msg, c.a1, c.a2, c.result, got)
+		}
+	}
+}
