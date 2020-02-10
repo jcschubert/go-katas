f0ef30be8d5b1392602032193d832aa4de3e24ec
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-10 15:36:58 +0100

    Max Length Difference: append, search max + min

--- maximumlengthdifference/difference.go
+++ maximumlengthdifference/difference.go
@@ -1,8 +1,21 @@
 package maximumlengthdifference
 
+import "math"
+
 func MxDifLg(a1 []string, a2 []string) int {
-	if len(a1) == 0 || len(a2) == 0 {
+	strings := append(a1, a2...)
+
+	if len(strings) == 0 {
 		return -1
 	}
-	return 0
+
+	max, min := 0.0, 0.0
+
+	for _, s := range strings {
+		len := float64(len(s))
+		max = math.Max(max, len)
+		min = math.Min(min, len)
+	}
+
+	return int(max - min)
 }
